2059de214c4cea7142b994953c40a4ade539e846
evgenii-sharonov <kamazobb2012@gmail.com>
2016-01-09 20:44:31 -0800

    1.2 + 1.3 (inside package)

--- for Petra`s/src/CalcInit.java
+++ for Petra`s/src/CalcInit.java
@@ -1,4 +1,5 @@
 
 public class CalcInit {
+	public static void main (???)
 
 }
---
+++ for Petra`s/src/task13/Point.java
@@ -0,0 +1,27 @@
+package task13;
+
+public class Point {
+	
+	private Point x;
+	private int y;
+	public Point (int x; int y) {
+		class S { 
+			Point p1 = new Point (0,0);
+			Point p2 = new Point (1,3);
+			Point p3 = new Point (3,1);
+			this.x = x;
+			this.y = y;  }
+		}
+		{
+		public int get Point x() {
+			return x;
+		}
+		double S = (1/2*a*b);
+		double a =   
+				// дальше формулу незнаю как написать в это поле (в каком виде)
+		
+		System.out.println (S);
+		
+	}
+
+
