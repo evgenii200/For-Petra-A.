313c252baedfd735985552032c2890cae2629dea
evgenii-sharonov <kamazobb2012@gmail.com>
2016-01-09 20:25:57 -0800

    task #1.2

---
+++ for Petra`s/src/Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+	double result () {
+		
+	}
+	public void add (double first, double second) {
+	
+	}
+	// по аналоги с substruct div multiple
+	
+System.out.println (result);
+}
+
+
+
+//Задание 2.
+//Нужно сделать файл Calculator.java и создать в нем класс Calculator. в классе должно быть внутреннее поле double result. 
+//и четыре метода add(double first, double second) по аналогии substruct div multiple. Методы должны быть void. 
+//Каждый метод вычисляет арифметическую операцию и записывает ее в поле result.
+//Добавить второй файл CalcInit. В нем сделать main метод и продемонстрировать использование класса Calculator.
